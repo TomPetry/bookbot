4c6b88c7eed47700f44577080ff09c60f3ce65cc
TomPetry <tom.petry@outlook.com>
2025-05-26 17:25:10 +0200

    Finished bookbot

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
 from stats import *
-
+import sys
 
 def get_book_text(file_path):
     with open(file_path) as f:
         return f.read()
 
-def main():
-    frankenstein = get_book_text("./books/frankenstein.txt")
-    words = get_num_words(frankenstein)
+def print_report(file_path):
+    frankenstein = get_book_text(file_path)
+    num_words = get_num_words(frankenstein)
     letters = get_num_chars(frankenstein)
-    print(f"{words} words found in the document")
-    print(letters)
+    sorted_letters = sort_and_restructure(letters)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {file_path}")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+
+    for letter in sorted_letters:
+        print(f"{letter['char']}: {letter['num']}")
+
+    print("============= END ===============")
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    print_report(sys.argv[1])
+
+
 
 main()
--- stats.py
+++ stats.py
@@ -10,3 +10,22 @@ def get_num_chars(text):
         else:
             letters[letter] = 1
     return letters
+
+def sort_and_restructure(letters):
+    sort_dicts = []
+    for letter in letters:
+
+        if not letter.isalpha():
+            continue
+
+        sort_dicts.append({
+            "char": letter,
+            "num": letters[letter]
+        })
+
+    sort_dicts.sort(reverse=True, key=sort_on)
+
+    return sort_dicts
+
+def sort_on(dict):
+    return dict["num"]
